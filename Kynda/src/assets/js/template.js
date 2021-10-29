2972faf1910aecb90456dc1d4c101e0296e7e76d
FrostyDog132 <nielsnoordzij2001@gmail.com>
2021-10-29 09:47:21 +0200

    Added more editing functions

--- Kynda/src/assets/js/template.js
+++ Kynda/src/assets/js/template.js
@@ -2,8 +2,26 @@
 // This function will handle all the changes that will be made on the template.
 function updateTemplate() 
 {
-    if(document.getElementById("middle-text").value != document.getElementById("template-text").value
-    && document.getElementById("template-text").value !== "") {
-        document.getElementById("middle-text").innerHTML = document.getElementById("template-text").value;
+    var titleText = document.getElementById("header-text");
+    var middleText = document.getElementById("middle-text");
+    var bottomText = document.getElementById("bottom-text");
+
+    var templateTitleInput = document.getElementById("template-title").value;
+    var templateMiddleInput = document.getElementById("template-text").value;
+    var templateBottomInput = document.getElementById("template-textfoto").value;
+
+    if(titleText.value != templateTitleInput && templateTitleInput !== "")
+    {
+        titleText.innerHTML = templateTitleInput;
+    }
+
+    if(middleText.value != templateMiddleInput && templateMiddleInput !== "") 
+    {
+        middleText.innerHTML = templateMiddleInput;
+    }
+
+    if(bottomText.value != templateBottomInput && templateBottomInput !== "") 
+    {
+        bottomText.innerHTML = templateBottomInput;
     }
 }
