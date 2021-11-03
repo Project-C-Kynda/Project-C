44d7d0faf7156575d4b1aa2f4078759cb7d8f560
JasmijnKramer <JasmijnKramer@users.noreply.github.com>
2021-11-03 13:43:10 +0100

    Added form for photo uploading and added styling for from

--- Kynda/src/app/app-routing.module.ts
+++ Kynda/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FotoUploadComponent } from './foto-upload/foto-upload.component';
 import { TemplateComponent } from './template/template.component';
 
 const routes: Routes = [
   { path: '',
-    redirectTo: '/template',
+    redirectTo: '/photoUpload',
     pathMatch: 'full'
   },
-  {path: "template", component: TemplateComponent}
+  {path: "template", component: TemplateComponent},
+  {path: "photoUpload", component: FotoUploadComponent}
 ];
 
 @NgModule({
