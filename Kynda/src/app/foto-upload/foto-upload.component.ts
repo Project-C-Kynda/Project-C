4be7161a98dba7af7d04714a10f8835dd9fa4648
JasmijnKramer <JasmijnKramer@users.noreply.github.com>
2021-11-03 13:04:27 +0100

    Add foto upload component

---
+++ Kynda/src/app/foto-upload/foto-upload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foto-upload',
+  templateUrl: './foto-upload.component.html',
+  styleUrls: ['./foto-upload.component.css']
+})
+export class FotoUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
