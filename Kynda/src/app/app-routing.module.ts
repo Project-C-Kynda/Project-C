fcebec7bad8a5f08f5778206f941c1924bfd57cb
JasmijnKramer <JasmijnKramer@users.noreply.github.com>
2021-11-15 15:39:15 +0100

    Add login page

--- Kynda/src/app/app-routing.module.ts
+++ Kynda/src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { TemplateComponent } from './template/template.component';
 import { DatabaseComponent } from './database/database.component';
 import { ImageLibraryComponent } from './image-library/image-library.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '',
-    redirectTo: '/template',
+    redirectTo: '/login',
     pathMatch: 'full'
   },
   {path: "template", component: TemplateComponent},
   {path: "database", component: DatabaseComponent},
-  {path: "image-library", component: ImageLibraryComponent}
+  {path: "image-library", component: ImageLibraryComponent},
+  {path: "login", component: LoginComponent},
 ];
 
 @NgModule({
--- Kynda/src/app/app.module.ts
+++ Kynda/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { TemplateComponent } from './template/template.component';
 import { ImageLibraryComponent } from './image-library/image-library.component';
 import { DatabaseComponent } from './database/database.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateComponent,
     ImageLibraryComponent,
-    DatabaseComponent
+    DatabaseComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
