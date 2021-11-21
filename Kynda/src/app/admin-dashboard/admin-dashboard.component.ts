562c4a49176b9dc962c5b1834c5edf08ecf1004f
JasmijnKramer <JasmijnKramer@users.noreply.github.com>
2021-11-21 19:41:50 +0100

    Updated NPM

--- Kynda/src/app/admin-dashboard/admin-dashboard.component.ts
+++ Kynda/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Company } from '../database/models/company';
+import { RestService } from '../database/services/rest.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  page = 1;
+  pageSize = 4;
+  companies: any = [];
+  collectionSize = this.companies.length;
+
+  constructor(private restservice : RestService) { 
+    this.remapCompanies();
+  }
 
   ngOnInit(): void {
+  }
+
+  getCompanies() {
+    this.restservice.GetCompanies()
+    .subscribe(data => {
+      this.companies = data; 
+    })
+  }
+
+  remapCompanies() {
+    this.companies.slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
 
 }
