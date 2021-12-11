23a5280f1b063fa9332910275a5eb4b33e75d89c
JasmijnKramer <JasmijnKramer@users.noreply.github.com>
2021-12-11 11:53:25 +0100

    Set cookie for company admin

--- Kynda/src/app/companyadmin-dashboard/companyadmin-dashboard.component.ts
+++ Kynda/src/app/companyadmin-dashboard/companyadmin-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { RestService } from '../database/services/rest.service';
 
 @Component({
@@ -10,15 +11,22 @@ import { RestService } from '../database/services/rest.service';
 export class CompanyadminDashboardComponent implements OnInit {
 
   templates: any = [];
+  currentUser: any;
 
-  constructor(private restservice: RestService, private router: Router) { }
+  constructor(private restservice: RestService, private router: Router, private cookieservice: CookieService) {
+    this.currentUser = JSON.parse(cookieservice.get('user' || '{}'))[0];
+   }
 
   ngOnInit(): void {
     this.getTemplates();
+
+    if (this.currentUser == undefined || this.currentUser.roleid != 2)
+    {
+      this.router.navigate(['/no-access']);
+    }
   }
 
   getTemplates(companyId:string = "1") {
-    //JSON.parse(localStorage.getItem('user') || '{}').companyid
     console.log('running');
     this.restservice.GetTemplates(companyId)
     .subscribe(data => {
