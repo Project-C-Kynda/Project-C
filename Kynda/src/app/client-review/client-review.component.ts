bba96b5a10bc615eaa99a59d3c52a0b80ddaf3b1
JasmijnKramer <JasmijnKramer@users.noreply.github.com>
2021-12-11 11:20:13 +0100

    Set cookie and roleid correct for client-review

--- Kynda/src/app/client-review/client-review.component.ts
+++ Kynda/src/app/client-review/client-review.component.ts
@@ -4,6 +4,8 @@ import { Template, TEMPLATES } from "./template-dir"
 import { HttpClient } from '@angular/common/http';
 import { DownloadService } from '../template-download/download.service';
 import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,6 +15,7 @@ import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeUrl } from '@angular/platf
 })
 export class ClientReviewComponent implements OnInit {
 
+  currentUser: any;
   templates = TEMPLATES;
   downloadTemplates: Template[] = [];
   reviewTemplates: Template[] = [];
@@ -23,11 +26,17 @@ export class ClientReviewComponent implements OnInit {
   httpString : any;
   htmlString: any;
 
-  constructor(private restservice : RestService, private http : HttpClient, private download:DownloadService, private sanitizer:DomSanitizer) { 
+  constructor(private restservice : RestService, private http : HttpClient, private cookieService: CookieService, private download:DownloadService, private sanitizer:DomSanitizer, private router: Router) {
+    this.currentUser = JSON.parse(this.cookieService.get('user') || '{}')[0];
   }
 
   ngOnInit(): void {
     this.splitTemplates();
+
+    if (this.currentUser == undefined || this.currentUser.roleid != 2)
+    {
+      this.router.navigate(['/no-access']);
+    }
     }
 
   splitTemplates(){
