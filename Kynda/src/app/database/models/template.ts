e15d31d090e147c777c8ff6c3cbaa5028fdaec8d
FrostyDog132 <nielsnoordzij2001@gmail.com>
2021-12-13 14:11:00 +0100

    Changed template model and updated function naming

--- Kynda/src/app/database/models/template.ts
+++ Kynda/src/app/database/models/template.ts
@@ -1,9 +1,8 @@
 export class Template {
     companyid!: number;
     name!: string;
-    templateref!: string;
     downloads!: number;
     price!: number;
-    approved!: boolean;
     templatetype!: string;
+    status!: string;
 }
