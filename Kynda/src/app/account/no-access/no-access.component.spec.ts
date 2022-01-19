277ae1799fb6dc3e226e190a707e075f3c0fdcf7
JasmijnKramer <JasmijnKramer@users.noreply.github.com>
2022-01-19 11:59:08 +0100

    Add unit test to app.component and login.component, deleted tests no-access.component & account.component

--- Kynda/src/app/account/no-access/no-access.component.spec.ts
+++ Kynda/src/app/account/no-access/no-access.component.spec.ts
@@ -18,8 +18,4 @@ describe('NoAccessComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
